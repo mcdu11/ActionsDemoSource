528221af9fe0c9e83f6d490007d80163bd7b7f30
duwenqiang <duwenqiang@alibaba-inc.com>
2022-02-28 23:42:43 +0800

    chore: welcome

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,10 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome !
         </a>
       </header>
     </div>
