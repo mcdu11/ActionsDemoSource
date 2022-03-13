22de58fe54019556a49638a822f723d840aeac28
duwenqiang <duwenqiang@alibaba-inc.com>
2022-03-13 22:51:12 +0800

    chore: change website link

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello Guest!
         </p>
         <a
           className="App-link"
           target="_blank"
-          href="https://www.baidu.com"
+          href="http://www.mcdu11.top"
           rel="noopener noreferrer"
         >
           Welcome to Mcdu11.top
